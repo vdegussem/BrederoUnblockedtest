d99131c89cd96f79ff60ba1ac90ac9e193a02c2b
vdegussem <vinginniodegussem@koraaledu.nl>
2026-05-08 02:15:43 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
 const cors_proxy = require('cors-anywhere');
 
 const host = '0.0.0.0';
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 10000; 
 
 cors_proxy.createServer({
-    originWhitelist: [], // Laat alle websites toe
-    requireHeader: [],   // VERWIJDERT de verplichting voor speciale headers (DIT IS DE FIX)
+    originWhitelist: [], // Staat alles toe
+    requireHeader: [],   // DIT IS DE CRUCIALE FIX: Staat browser-verzoeken toe
     removeHeaders: [
         'cookie',
         'cookie2',
         'x-frame-options',
         'content-security-policy',
         'set-cookie'
-    ],
-    handleOptions: true
+    ]
 }).listen(port, host, function() {
-    console.log('Proxy is gefixt en draait op poort ' + port);
+    console.log('De BrederoUnblocked proxy draait op poort ' + port);
 });
